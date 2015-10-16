64842e4137ba00fa852184d2aa4f4653da9e8997
Marc Henry de Frahan (lab) <marchdf@umich.edu>
2015-10-16 10:11:47 -0400

    Cleaned up directory, added a gitignore file

--- sh_latex.sh
+++ sh_latex.sh
@@ -1,8 +1,8 @@
-rm report.bbl;
-rm report.aux;
+rm -f *.aux  *.bbl  *.blg  *.dvi  *.log  *.out  *.ps
 pdflatex report;
 pdflatex report;
 bibtex report;
 pdflatex report;
 pdflatex report;
+rm -f *.aux  *.bbl  *.blg  *.dvi  *.log  *.out  *.ps
 #evince report.pdf;
